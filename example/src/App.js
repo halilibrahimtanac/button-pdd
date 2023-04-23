06211c12ecf1c3ab0529951353dc619481c17abf
halilibrahimtanac <halilibrahimtanac21@outlook.com>
2023-04-23 12:14:36 +0300

    Initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,22 @@ import { Button } from 'button-pdd'
 import 'button-pdd/dist/index.css'
 
 const App = () => {
-  return <Button type='primary'>click me</Button>
+  return (
+    <div
+      style={{
+        width: '400px',
+        height: '50px',
+        display: 'flex',
+        justifyContent: 'space-between'
+      }}
+    >
+      <Button type='primary'>primary</Button>
+      <Button type='default'>default</Button>
+      <Button type='dashed'>dashed</Button>
+      <Button type='text'>text</Button>
+      <Button type='link'>link</Button>
+    </div>
+  )
 }
 
 export default App
